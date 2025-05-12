238bb8293919175f9933a77af3334a1ec958ff60
hosung77 <ho4728@gmail.com>
2025-05-12 09:49:56 +0900

    refactor: 1-3 해결

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryQuery.java
@@ -0,0 +1,14 @@
+package org.example.expert.domain.todo.repository;
+
+
+import org.example.expert.domain.todo.dto.response.TodoResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+public interface TodoRepositoryQuery {
+
+    Page<TodoResponse> searchByCondition(Pageable pageable, LocalDate start, LocalDate end, String weather);
+
+}
